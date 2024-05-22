21d94b4d0da60a4d5c16814e74a8de6661f0f3aa
Fadzaiwenymuzadzi <fadzaiwendym@gmail.com>
2024-05-22 12:32:04 -0700

    all tasks

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,35 +1,40 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """ Python caching systems """
-
 from base_caching import BaseCaching
+from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
-    """ LRU caching system """
-
+    """ MRU caching system
+    """
     def __init__(self):
-        ''' Initialize class instance. '''
+        """ Initialize class instance. """
         super().__init__()
-        self.current_keys = []
+        self.cache_data = OrderedDict()
+        self.mru = ""
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None or item is not None:
-            self.cache_data[key] = item
-            if key not in self.current_keys:
-                self.current_keys.append(key)
+        """ Add an item in the cache
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.cache_data.update({key: item})
+                    self.mru = key
+                else:
+                    # discard the most recently used item
+                    discarded = self.mru
+                    del self.cache_data[discarded]
+                    print("DISCARD: {}".format(discarded))
+                    self.cache_data[key] = item
+                    self.mru = key
             else:
-                self.current_keys.append(self.current_keys.pop(
-                    self.current_keys.index(key)))
-            if len(self.current_keys) > BaseCaching.MAX_ITEMS:
-                discarded_key = self.current_keys.pop(-2)
-                del self.cache_data[discarded_key]
-                print('DISCARD: {}'.format(discarded_key))
+                self.cache_data[key] = item
+                self.mru = key
 
     def get(self, key):
-        """ Get an item by key """
-        if key is not None and key in self.cache_data:
-            self.current_keys.append(self.current_keys.pop(
-                self.current_keys.index(key)))
-            return self.cache_data.get(key)
-        return None
+        """ Get an item by key
+        """
+        if key in self.cache_data:
+            self.mru = key
+            return self.cache_data[key]
